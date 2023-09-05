4c22011dd1cdc8d64e77f9af005f3b3402ed938b
naekkoch <micaelaas3105@gmail.com>
2023-09-05 19:05:02 -0300

    socorro

---
+++ index.js
@@ -0,0 +1,52 @@
+/*
+let i = 10;
+
+while(i> 1){
+console.log(i)
+i--;
+}
+
+
+let numeros = [];
+
+const chamarPrompt = () => {
+    const numero = Number(prompt("Digite um número"));
+numeros.push(numero);
+};
+while (!numeros.includes(0)){
+    chamarPrompt();
+}
+if (numeros.includes(0)) {
+    let i = 0;
+    let soma = 0;
+    while (i < numeros.length - 1) {
+        soma = soma + numeros[i];
+        i++;
+    }
+    console.log(soma)}
+    
+    
+    exercicio 1: Será imprimido 10 no console
+
+    exercicio 2: a) 19, 21, 23, 25, 27, 30 será imprimido
+    b) While
+
+    exercicio 3: 5 *****
+
+exercicio 1 escrita:
+const animal = prompt ("Quantos bichinhos de estimação vc tem?")
+if(animal <= 0)
+console.log("Que pena!Você pode adotar um pet")
+
+else(animal >=1) ;{
+const pergunta = prompt ("Quais são os nomes?")
+console.log(pergunta)
+}
+
+exercicio 2 escrita:
+*/
+const valorOriginal = [0, 1, 2 , 3 ,4 ,5 ,6, 7 ,8 ,10]
+const valor = ()=> {
+for(let valor of valorOriginal){
+console.log(valorOriginal)}
+const valorMaximo = Number(valorMaximo > valorOriginal)}
