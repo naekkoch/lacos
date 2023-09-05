af01f5b8cc951746af8ac2e1a3f76308c5984f53
naekkoch <micaelaas3105@gmail.com>
2023-09-05 19:40:40 -0300

    arrumei

--- index.js
+++ index.js
@@ -34,19 +34,24 @@ if (numeros.includes(0)) {
     exercicio 3: 5 *****
 
 exercicio 1 escrita:
-const animal = prompt ("Quantos bichinhos de estimação vc tem?")
-if(animal <= 0)
-console.log("Que pena!Você pode adotar um pet")
-
-else(animal >=1) ;{
-const pergunta = prompt ("Quais são os nomes?")
-console.log(pergunta)
-}
-
-exercicio 2 escrita:
 */
+const animal = prompt("Quantos bichinhos de estimação vc tem?");
+if (animal <= 0) {
+  console.log("Que pena!Você pode adotar um pet");
+} else {
+  let nomes = [];
+  for (i = 0; i <= animal; i++) {
+    const pergunta = prompt("Quais são os nomes?");
+    nomes.push(pergunta);
+  }
+  console.log(nomes);
+}
+/*
+exercicio 2 escrita:
+
 const valorOriginal = [0, 1, 2 , 3 ,4 ,5 ,6, 7 ,8 ,10]
 const valor = ()=> {
 for(let valor of valorOriginal){
 console.log(valorOriginal)}
 const valorMaximo = Number(valorMaximo > valorOriginal)}
+*/
